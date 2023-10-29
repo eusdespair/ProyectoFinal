65647a983ae62a86f4ed967f690551289b08d7ae
eusdespair <145882604+eusdespair@users.noreply.github.com>
2023-10-28 20:42:37 -0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// JavaScript para mostrar u ocultar el menú al hacer clic en el botón de hamburguesa
+document.querySelector('.menuToggle').addEventListener('click', function() {
+
+    document.querySelector('.navLinks').classList.toggle('active');
+    
+}
+
+);
